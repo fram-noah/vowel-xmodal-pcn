b867b04d2a087c7592c7390300b3ab7c2483644f
fram-noah <nfram@ccrma.stanford.edu>
2026-09-05 13:16:45 -0500

    Updated test script and uv environment.

--- tests/test.py
+++ tests/test.py
@@ -7,3 +7,23 @@ import pytest
 import numpy
 
 # Things we need tests for [PLEASE ADD TO LIST!]:
+#   1. Model structure is connected
+#   2. Input dimensions match signal dimensions
+#   3. Every frame has exactly one audio window matched to it
+#   4. No computation errors in audio spectrograms
+#   5. Video conversion to greyscale runs properly (or color handling is correct)
+
+class TestInputs:
+    def test_windowmatch(self):
+
+    def test_audiospec(self):
+
+    def test_greyscale(self):
+
+class TestModel:
+    def test_connected(self):
+
+    def test_dimensionmatch(self):
+
+# We can also add tests here that look at output parameters, but I've kept things
+# to the model structure itself for now.
